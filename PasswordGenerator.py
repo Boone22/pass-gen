3e8b99a3b80aae05945f9075e279ec06cc9b50be
Connor Hillerby <chillerby@googlemail.com>
2018-08-27 17:20:45 +0100

    fixing up stuff

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -2,8 +2,6 @@
 
 import secrets
 
-
-
 def user_pass_length():
 	validtype = False
 	validlength = False
@@ -29,50 +27,49 @@ def user_pass_length():
 	placeholder = input("Press again to exit")
 
 def user_upcase():
-	validans = True
-	while validans:		
+	invalidans = True
+	while invalidans:		
 		upcase = input("Would you like your password to have uppercase letters?(y/n)\n")
 		if upcase.lower() == "y":
+			invalidans = False
 			upcase_final = True
-			validans = False
 		elif upcase.lower() == "n":
+			invalidans = False
 			upcase_final = False
-			validans = False
 		else:
 			print("Sorry that is not a valid answer, please try again.")
-			validans = True
+			invalidans = True
 	return upcase_final
 
 def user_integer():
-	validans = True
-	while validans:		
+	invalidans = True
+	while invalidans:		
 		raw = input("Would you like your password to have numbers?(y/n)\n")
 		if raw.lower() == "y":
+			invalidans = False
 			integer_final = True
-			validans = False
 		elif raw.lower() == "n":
+			invalidans = False
 			integer_final = False
-			validans = False
 		else:
 			print("Sorry that is not a valid answer, please try again.")
-			validans = True
+			invalidans = True
 	return integer_final
 
 def user_spec():
-	validans = True
-	while validans:		
+	invalidans = True
+	while invalidans:		
 		raw = input("Would you like your password to have special characters?(y/n)\n")
 		if raw.lower() == "y":
+			invalidans = False
 			spec_final = True
-			validans = False
 		elif raw.lower() == "n":
+			invalidans = False
 			spec_final = False
-			validans = False
 		else:
-			print("Sorry that is not a valid answer, please try again.")
-			validans = True
+			print("Sorry that is not a valid answer. Please try again.")
+			invalidans = True
 	return spec_final
-
 
 alpha_up = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 alpha_down = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
@@ -83,21 +80,30 @@ spec_list = ["?", "!", "&", "$", "#"]
 # random number 1 - 4 to reference each list through indexing a list with those lists inside of it 
 
 def main():
-	char_list = [alpha_up, alpha_down, integer_list, spec_list]
-	if upcase_final and integer_final and spec_final:
-		print("Your final password is: \n")
+	print("Your final password is: \n")
 	for i in range(0, pass_length):
-		temp = char_list[secrets.randbelow(4)]
+		temp = char_list[secrets.randbelow(len(char_list))]
 		temp = temp[secrets.randbelow(len(temp))]
 		print(temp, end="")
+	print("\npass_length =", pass_length, "\nupcase_final =", upcase_final, "\ninteger_final =", integer_final, "\nspec_final =", spec_final)
 	placeholder = input("\nplaceholder")
-
 
 pass_length = user_pass_length()
 upcase_final = user_upcase()
 integer_final = user_integer()
 spec_final = user_spec()
-print(pass_length, " ", upcase_final, " ", integer_final, " ", spec_final)
+params = [upcase_final, integer_final, spec_final]
+params_dict = {upcase_final:alpha_up}
+char_list = [alpha_down]
+counter = 0
+# for i in params_dict:
+# 	counter = counter + 1
+# 	if i == True:
+# 		char_list.append(params_dict.values())
+# 	print(char_list)
+for k in params_dict.items():
+	print(params_dict.items())
+	print(k)
 main()
 print("\nThank you for using Connor's password generator!")
 
