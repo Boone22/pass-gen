cdab9c128979b2197e7df8bd76974c4979b147ff
Connor Hillerby <chillerby@googlemail.com>
2018-09-15 16:39:48 +0100

    Finally functional

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -2,29 +2,34 @@
 
 import secrets
 
+alpha_up = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+alpha_down = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+integer_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+spec_list = ["?", "!", "&", "$", "#"]
+
 def user_pass_length():
-	validtype = False
-	validlength = False
-	while validtype == False:	# evaluates whether the user input is an integer
-		try:
-			pass_length = int(input("How long would you like your password to be?(<17)\n"))
-			validtype = True
-		except ValueError:
-			print("I'm sorry that is not an integer, please try again")	
-	for x in range(1, 17):	# evaluates user input password length
-		if x == pass_length:
-			print("That is a GOOD number")
-			validlength = True
-			break
+	invalidans = True
+	while invalidans:
+		validtype = False
+		validlength = False
+		while validtype == False:	# evaluates whether the user input is an integer
+			try:
+				pass_length = int(input("How long would you like your password to be?(<17)\n"))
+				validtype = True
+			except ValueError:
+				print("I'm sorry, that is not an integer, please try again")	
+		for x in range(1, 17):	# evaluates user input password length
+			if x == pass_length:
+				validlength = True
+				break
+			else:
+				validlength = False	
+		if validtype and validlength:	# evaluates both booleans for a final decision
+			invalidans = False
 		else:
-			validlength = False	
-	if validtype and validlength:	# evaluates both booleans for a final decision
-		print("Completed.")
-	else:
-		print("I'm sorry but that is not a valid password length, please try again")
-		user_pass_length()
+			print("I'm sorry but that is not a valid password length, please try again")
+			invalidans = True
 	return pass_length
-	placeholder = input("Press again to exit")
 
 def user_upcase():
 	invalidans = True
@@ -71,39 +76,24 @@ def user_spec():
 			invalidans = True
 	return spec_final
 
-alpha_up = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-alpha_down = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-integer_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-spec_list = ["?", "!", "&", "$", "#"]
-
-# random number - index the list
-# random number 1 - 4 to reference each list through indexing a list with those lists inside of it 
-
 def main():
-	print("Your final password is: \n")
+	print("Your final password is:")
 	for i in range(0, pass_length):
-		temp = char_list[secrets.randbelow(len(char_list))]
-		temp = temp[secrets.randbelow(len(temp))]
+		temp = char_list[secrets.randbelow(len(char_list))]		# indexing char_list for one of the above 4 lists
+		temp = temp[secrets.randbelow(len(temp))]				# generating random number below the length of the list just chosen and indexes it
 		print(temp, end="")
-	print("\npass_length =", pass_length, "\nupcase_final =", upcase_final, "\ninteger_final =", integer_final, "\nspec_final =", spec_final)
-	placeholder = input("\nplaceholder")
 
 pass_length = user_pass_length()
-upcase_final = user_upcase()
-integer_final = user_integer()
-spec_final = user_spec()
-params = [upcase_final, integer_final, spec_final]
-params_dict = {upcase_final:alpha_up}
 char_list = [alpha_down]
-counter = 0
-# for i in params_dict:
-# 	counter = counter + 1
-# 	if i == True:
-# 		char_list.append(params_dict.values())
-# 	print(char_list)
-for k in params_dict.items():
-	print(params_dict.items())
-	print(k)
+
+if user_upcase():
+	char_list.append(alpha_up)
+
+if user_integer():
+	char_list.append(integer_list)
+
+if user_spec():
+	char_list.append(spec_list)
+
 main()
 print("\nThank you for using Connor's password generator!")
-
--- RunPasswordGenerator.bat
+++
@@ -1,1 +0,0 @@
-python PasswordGenerator.py
